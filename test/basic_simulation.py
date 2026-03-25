73ac4c9e24f6b49618a56c0f4efd8032c9b141d5
joelbarron06 <joelm_barron@outlook.com>
2026-03-25 12:16:22 +0000

    updates for renamed parameters

--- test/basic_simulation.py
+++ test/basic_simulation.py
@@ -5,12 +5,10 @@ import os
 def main():
 
     system = SolarSystem()
-    system.read_parameters("parameters_solar.json")
+    system.read_parameters("base_parameters.json")
     system.run_simulation()
-    fig, ax = system.animation()
+    fig, ax, ani = system.animation()
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    plt.savefig(os.path.join(BASE_DIR, "figures", "basic_animation"))
     plt.show()
 
 if __name__ == "__main__":
