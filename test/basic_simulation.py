8f3e3b94f29b00f5e95bbafe1bc9ed5b911b3f35
joelbarron06 <joelm_barron@outlook.com>
2026-03-25 12:01:02 +0000

    adapted code to save basic animamation

--- test/basic_simulation.py
+++ test/basic_simulation.py
@@ -1,5 +1,6 @@
 from src.system import SolarSystem
 import matplotlib.pyplot as plt
+import os
 
 def main():
 
@@ -8,6 +9,8 @@ def main():
     system.run_simulation()
     fig, ax = system.animation()
 
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    plt.savefig(os.path.join(BASE_DIR, "figures", "basic_animation"))
     plt.show()
 
 if __name__ == "__main__":
