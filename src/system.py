69d5ed77230633556cdab13e1d3b56e5873f34ae
joelbarron06 <joelm_barron@outlook.com>
2026-03-30 11:45:53 +0100

    fixed logic calculating G

--- src/system.py
+++ src/system.py
@@ -81,7 +81,7 @@ class SolarSystem:
         # store mass to initalise velocity
         star_mass = file_stars[0]["mass"]
         # custom graviatational constant by unit conversion
-        self.G = (4 * np.pi**2) / (star_mass + 1)
+        self.G = (4 * np.pi**2) / (332946.0 + 1)
         
         # add bodies in file into system
         for file_body in parameters_solar["bodies"]:
